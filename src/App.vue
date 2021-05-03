bc0c374e3e688c9c868260e997f72ce5df7d1e5f
Wasif Sarker <w.sarker@alumni.ubc.ca>
2021-05-03 11:19:48 -0700

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+
+  <v-app>
+         
+
+    <ToolBar/>
+    <v-content>
+      <router-view/>
+    </v-content>
+    <Footer/>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Footer from "./components/Footer";
+import ToolBar from "./components/ToolBar";
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+components: {
+    
+    Footer,
+    ToolBar,
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  },
+
+
+  data: () => ({
+    //
+  }),
+};
+</script>
